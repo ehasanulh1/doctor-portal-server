a59877210a69ed9996edc135ee6d9ec6339735d4
ehasanulh1 <ehasanulh1@gmail.com>
2023-07-01 20:18:44 +0600

    added bookings query

--- index.js
+++ index.js
@@ -104,6 +104,14 @@ async function run() {
         * app.delete('/bookings/:id')
         */
 
+
+       app.get('/bookings', async (req, res)=>{
+        const email = req.query.email;
+        const query = {email: email}
+        const bookings = await bookingCollection.find(query).toArray();
+        res.send(bookings)
+       })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking)
